2369788f1d72b60b7e090815f99117ab3cc8ef41
Dynatrace Developer <dynatrace@VM-001.localdomain>
2015-05-07 17:46:59 +0000

    Maven Original Add

---
+++ src/main/java/cdelivery_demo/Excercise.java
@@ -0,0 +1,15 @@
+package cdelivery_demo;
+
+public class Excercise {
+
+	public void RunDemoDBFunctions() 
+	{
+		
+	}
+	
+	public void RunDemoWebServiceCalls()
+	{
+		
+	}
+	
+}
---
+++ src/main/java/cdelivery_demo/ExcerciseTest.java
@@ -0,0 +1,16 @@
+package cdelivery_demo;
+
+
+import junit.framework.TestCase;
+
+public class ExcerciseTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
