6a877b97d8e08d564d29f946a6c0589b29080c74
bhdynatrace <brett.hofer@dynatrace.com>
2015-05-08 13:59:59 +0000

    Updated Test Cases

--- src/main/java/cdelivery_demo/ExcerciseTest.java
+++ src/main/java/cdelivery_demo/ExcerciseTest.java
@@ -1,16 +1,27 @@
 package cdelivery_demo;
 
-
+import cdelivery_demo.Excercise;	
 import junit.framework.TestCase;
 
 public class ExcerciseTest extends TestCase {
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
+	}
+	
+	public void testExcercise()
+	{
+		Excercise ex = new Excercise();
+		
+		ex.RunDemoDBFunctions();
+		ex.RunDemoProblem1();
+		ex.RunDemoWebServiceCalls();
+		
 	}
 
 }
